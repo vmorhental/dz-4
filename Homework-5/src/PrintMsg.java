6d7a1b95d7d7ef7c1c36a18e48374304ecf47c80
vmorhental <128645413+vmorhental@users.noreply.github.com>
2023-03-30 20:35:27 +0300

    Update PrintMsg.java

--- Homework-5/src/PrintMsg.java
+++ Homework-5/src/PrintMsg.java
@@ -1,8 +1,9 @@
 public class PrintMsg {
     public static String printAndReturn(){
-        String a = "---";
-        int b = 10;
-        String result = a + b + a;
+        String a = "---<";
+        String b = ">---";
+        int c = 10;
+        String result = a + c + b;
         System.out.println(result);
         return result;
     }
